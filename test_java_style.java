7fdd402da5818d54a4b742dc5045eae33452a42a
wanpengfei <wanpengfei91@163.com>
2020-06-18 19:37:46 +0800

    test

--- test_java_style.java
+++ test_java_style.java
@@ -18,7 +18,7 @@ public class Application {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+	public commandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
 			System.out.println("Let's inspect the beans provided by Spring Boot:"); 
 
